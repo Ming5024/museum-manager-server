657f0b97ad5af7d09a633f62029137bd30e3521a
barrymbxu(许铭彬) <barrymbxu@tencent.com>
2018-11-27 20:18:33 +0800

    增加科大讯飞语音合成接口

--- backendManager/views.py
+++ backendManager/views.py
@@ -6,14 +6,20 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
 
 import json
+import requests
+import time
+import hashlib
+import base64
+import os
+from urllib import urlencode
 
 # Create your views here.
 @csrf_exempt
 def getSpecies(request):
-    print(request.body)
+    # print(request.body)
     recv_data = json.loads(request.body.decode('utf-8'))
     dateRange = recv_data['dateRange']
-    print(dateRange)
+    # print(dateRange)
     send_data = {}
 
     send_data['userNum'] = 1000
@@ -49,6 +55,74 @@ def getAges(request):
         {"value": 353, "name": '40-60岁'},
         {"value": 1353, "name": '60岁以上'},
     ]
+
+    response = HttpResponse(json.dumps(send_data), content_type="application/json")
+    response['Access-Control-Allow-Origin'] = '*'       # 跨域头部
+    return response
+
+def writeFile(file, content):
+    with open(file, 'wb') as f:
+        f.write(content)
+    f.close()
+
+@csrf_exempt
+def getAudio(request):
+    print(request.body)
+    # recv_data = json.loads(request.body.decode('utf-8'))
+    # text = recv_data['text']
+    send_data = {}
+
+    URL = "http://api.xfyun.cn/v1/service/v1/tts"
+    AUE = "raw"
+    APPID = "5bfd079b"
+    API_KEY = "c978a5902ca5d233edbe4e721c71a842"
+
+    curTime = str(int(time.time()))
+    #ttp=ssml
+    param = "{\"aue\":\""+AUE+"\",\"auf\":\"audio/L16;rate=16000\",\"voice_name\":\"xiaoyan\",\"engine_type\":\"intp65\"}"
+    print("param:{}".format(param))
+    paramBase64 = str(base64.b64encode(param.encode('utf-8')))
+    print("x_param:{}".format(paramBase64))
+    
+    m2 = hashlib.md5()
+    m2.update((API_KEY + curTime + paramBase64).encode('utf-8'))
+    
+    checkSum = m2.hexdigest()
+    print('checkSum:{}'.format(checkSum))
+    
+    header ={
+            'X-CurTime':curTime,
+            'X-Param':paramBase64,
+            'X-Appid':APPID,
+            'X-CheckSum':checkSum,
+            'X-Real-Ip':'127.0.0.1',
+            'Content-Type':'application/x-www-form-urlencoded; charset=utf-8',
+    }
+    print(header)
+    
+    # with open("E:/museumServer/123.txt", 'r') as f:
+    #     text = f.read()
+
+    print(os.path.abspath(".."))
+    with open("123.txt", "r") as f:
+        text = f.read()
+
+    print({"text": unicode(text, "gb2312")})
+    r = requests.post(URL,headers=header,data={"text": unicode(text, "gb2312").encode('utf-8')})
+    contentType = r.headers['Content-Type']
+    if contentType == "audio/mpeg":
+        sid = r.headers['sid']
+        send_data['audio'] = base64.b64encode(r.content)
+        # if AUE == "raw":
+        #     writeFile(sid+".wav", r.content)
+        # else :
+        #     writeFile("xiaoyan"+".mp3", r.content)
+        print ("success, sid = " + sid)
+    else :
+        send_data['result'] = "failed"
+        print (r.text)
+    # print(r.content)
+    
 
     response = HttpResponse(json.dumps(send_data), content_type="application/json")
     response['Access-Control-Allow-Origin'] = '*'       # 跨域头部
